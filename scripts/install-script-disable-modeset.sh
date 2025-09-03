e8c3f2590f68a9d27ef387bf3b8dc628e22b2409
Slawek <slawek@cloudrift.ai>
2025-09-03 15:36:38 -0700

    Disable modeset script

---
+++ scripts/install-script-disable-modeset.sh
@@ -0,0 +1,31 @@
+# 1) Drop-in that appends your params
+cat >/etc/default/grub.d/98-extra-cmdline.cfg <<'EOF'
+EXTRA_CMDLINE="module_blacklist=nvidia_drm,nvidia_modeset nouveau.modeset=0 pci=realloc pci=pcie_bus_perf"
+GRUB_CMDLINE_LINUX_DEFAULT="${GRUB_CMDLINE_LINUX_DEFAULT:+$GRUB_CMDLINE_LINUX_DEFAULT }$EXTRA_CMDLINE"
+GRUB_CMDLINE_LINUX="${GRUB_CMDLINE_LINUX:+$GRUB_CMDLINE_LINUX }$EXTRA_CMDLINE"
+EOF
+
+sudo tee /etc/default/grub.d/00-disable-os-prober.cfg >/dev/null <<'EOF'
+GRUB_DISABLE_OS_PROBER=true
+EOF
+
+cat >/etc/modprobe.d/99-nvidia-compute-only.conf <<'EOF'
+# Block display-related pieces
+blacklist nvidia_drm
+blacklist nvidia_modeset
+# (belt-and-suspenders) ensure KMS stays off if nvidia-drm ever loads
+options nvidia-drm modeset=0
+
+# Just in case: never load nouveau either
+blacklist nouveau
+options nouveau modeset=0
+EOF
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ubuntu --removable
+
+update-initramfs -u -k all
+
+update-grub
+
+#sed -i 's|set prefix=(\$root)'\''/grub'\''|set prefix=(\$root)'\''/boot/grub'\''|' /boot/efi/EFI/ubuntu/grub.cfg
+

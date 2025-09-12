c8e33f1d3ae75e06476b7727a48d4efe1cb2b26f
Slawek <slawek@cloudrift.ai>
2025-09-12 00:53:20 +0000

    nvidia fixes

--- python/nvidia.py
+++ python/nvidia.py
@@ -23,7 +23,11 @@ def remove_nvidia_driver():
     # Check if the nvidia module is loaded
     if check_nvidia():
         print("NVIDIA driver is in use. Attempting to remove it.")
-        
+
+        # Run the NVIDIA uninstaller (if it exists)
+        print("Running NVIDIA uninstaller...")
+        run(["/usr/bin/nvidia-uninstall", "-s"])
+
         # Remove NVIDIA driver and associated packages
         print("Removing NVIDIA packages...")
         run(["apt-get", "remove", "--purge", "^nvidia-.*"])
@@ -36,12 +40,8 @@ def remove_nvidia_driver():
 
         # Clean up X11 configuration
         print("Removing X11 configuration files...")
-        run(["rm", "/etc/X11/xorg.conf"])
+        #run(["rm", "/etc/X11/xorg.conf"])
         run(["rm", "-rf", "/etc/X11/xorg.conf"])
-
-        # Run the NVIDIA uninstaller (if it exists)
-        print("Running NVIDIA uninstaller...")
-        run(["/usr/bin/nvidia-uninstall"])
 
         # Clean up modprobe configurations
         print("Removing modprobe configuration files...")
